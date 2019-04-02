83aedbaded768624fde5a4be10a56335319944a2
Reodor Felgen <felgen@klypa.ntnu.no>
2019-04-02 12:07:06 +0200

    Modified stateSwitch in esm and setPriorityDirection in orders

--- rammeverk/source/esm.c
+++ rammeverk/source/esm.c
@@ -8,13 +8,25 @@
 void esm_stateSwitch(state CurrentState){
 	switch (CurrentState){
 		case Waiting_for_init:
-			updateOrders
-			if ("getOrders"){ //tar bestilling
-				"goToPriorityOrder" //får heisen til å bevege seg mot prioritert bestilling
-				
-			}
+			CurrentState=Not_moving_at_floor;			
 		case Not_moving_at_floor:
+			elev_set_motor_direction(DIRN_STOP);
+			elev_set_door_open_lamp(1);
+			//Timerfunksjon inn her
+			//husk å resette timer
+			elev_set_door_open_lamp(0);
+			orders_updateOrderMatrix(); //tar bestilling
+			//får heisen til å bevege seg mot prioritert bestilling
+			while(orders_setPriorityDirection() == -1){
+				orders_updateOrderMatrix();
+				}
+			if (!orders_setPriorityDirection()){
+				CurrentState = Not_moving_at_floor;
+				break;
+			} 
+			CurrentState = Moving;
 		case Moving:
+
 		case At_floor:
 		case Not_moving_between_floors:
 
--- rammeverk/source/orders.c
+++ rammeverk/source/orders.c
@@ -1,11 +1,41 @@
 #include "orders.h"
+#include "elev.h"
+#include "io.h"
 
 
 
-{
-	for (int i=0, i<N_FLOORS, i++){
-		for (int i=0, i<3,i++){
-			elev_get_button_signal(j,i);
+
+void orders_updateOrderMatrix(){
+	for (int i=0; i<N_FLOORS; i++){
+		for (int j=0; j<N_BUTTONS;j++){ 
+			if(elev_get_button_signal(i,j))
+				order_matrix[i][j] = 1;
 		}
 	}
+}
+
+//Returnerer -1 hvis ingen bestillinger, 0 hvis bestilling i samme 
+//etasje og 1 hvis det er i en annen etasje
+int orders_setPriorityDirection() {
+	//Stoppknappfunksjon
+	int CurrentFloor = elev_get_floor_sensor_signal();
+	int NumberOfOrders = 0;
+	for (int i = 0; i < N_FLOORS; ++i){
+		for (int j = 0; j < N_BUTTONS; ++j){
+			NumberOfOrders += order_matrix[i][j];
+		}
+	}
+	if (!NumberOfOrders){
+		elev_set_motor_direction(0); //Denne trengs nok egentlig ikke
+		return -1;
+	}
+	for (int i = 0; i < N_BUTTONS; ++i){
+		if (order_matrix[CurrentFloor][i]) {
+			elev_set_motor_direction(0);
+			//CurrentState = Not_moving_at_floor;
+			return 0;
+		}
+	}
+	//Legg inn logikk for når den skal kjøre opp og ned
+	//returner 1
 }
