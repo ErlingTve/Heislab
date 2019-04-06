42332362c66ffba23b7672204274a48f69d1cf94
Erling Tveter <erlingtv@stud.ntnu.no>
2019-04-06 14:59:54 +0200

    Added enum posisjon and global variable Posisjon + added some Doxygen comments

--- rammeverk/source/esm.h
+++ rammeverk/source/esm.h
@@ -16,4 +16,14 @@ typedef enum {WAITING_FOR_INIT, NOT_MOVING_AT_FLOOR, MOVING, AT_FLOOR, NOT_MOVIN
 */
 void esm_stateSwitch(state CurrentState);
 
+/**
+ @brief Enum for hvilken posisjon heisen er i
+ */
+typedef enum {FØRSTE, MELLOM_FØRSTE_OG_ANDRE, ANDRE, MELLOM_ANDRE_OG_TREDJE, TREDJE, MELLOM_TREDJE_OG_FJERDE, FJERDE} posisjon;
+
+/**
+ @brief Global variabel som sier hvilken posisjon heisen til enhver tid er i
+ */
+posisjon Posisjon;
+
 #endif
