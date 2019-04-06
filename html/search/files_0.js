064d43c85178ab576efd4f27f01fd474bc60182a
Erling Tveter <erlingtv@stud.ntnu.no>
2019-04-06 13:02:59 +0200

    Modified Doxygen-files

--- html/search/files_0.js
+++ html/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['elev_2eh',['elev.h',['../elev_8h.html',1,'']]],
   ['esm_2eh',['esm.h',['../esm_8h.html',1,'']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,9 +1,10 @@
 var indexSectionsWithContent =
 {
-  0: "eos",
+  0: "eost",
   1: "eo",
   2: "eo",
-  3: "s"
+  3: "e",
+  4: "st"
 };
 
 var indexSectionNames =
@@ -11,7 +12,8 @@ var indexSectionNames =
   0: "all",
   1: "files",
   2: "functions",
-  3: "enums"
+  3: "typedefs",
+  4: "enums"
 };
 
 var indexSectionLabels =
@@ -19,6 +21,7 @@ var indexSectionLabels =
   0: "All",
   1: "Files",
   2: "Functions",
-  3: "Enumerations"
+  3: "Typedefs",
+  4: "Enumerations"
 };
 
