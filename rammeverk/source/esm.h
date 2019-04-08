76a955108e5b05bb7b90075b20a183d031bafe09
Erling Tveter <erlingtv@stud.ntnu.no>
2019-04-08 14:58:44 +0200

    Fikset småbugs i sitchState

--- rammeverk/source/esm.h
+++ rammeverk/source/esm.h
@@ -8,11 +8,10 @@
 /**
  @brief definerer navn på de forskjellige tilstandende til tilstandsmaskinen
  */
-typedef enum {WAITING_FOR_INIT, NOT_MOVING_AT_FLOOR, MOVING, AT_FLOOR,EMERGENCY-STOP, NOT_MOVING_BETWEEN_FLOORS} state;
+typedef enum {WAITING_FOR_INIT, NOT_MOVING_AT_FLOOR, MOVING, AT_FLOOR,EMERGENCY_STOP, NOT_MOVING_BETWEEN_FLOORS} state;
 
 /**
  @brief Endrer tilstander (i praksis tilstandsmaskinen)
- @param CurrentState Nåværende tilstand
 */
 void esm_stateSwitch(void);
 
@@ -25,6 +24,7 @@ typedef enum {FØRSTE = 0, ANDRE = 1, TREDJE = 2, FJERDE = 3, MELLOM_FØRSTE_OG_
  @brief Global variabel som sier hvilken posisjon heisen til enhver tid er i
  */
 posisjon Posisjon;
+
 /**
  @brief Privat variabel som sier hvilken state tilstandmaskinen er i
  */
