852479fb659bdcb15d55fa96f15400ff3a9bb98d
Erling Tveter <erlingtv@stud.ntnu.no>
2019-04-08 15:33:34 +0200

    Fixed bugs

--- rammeverk/source/timer.c
+++ rammeverk/source/timer.c
@@ -1,13 +1,11 @@
 #include "timer.h"
 
-<<<<<<< HEAD
-=======
+
 #include <stdio.h>
->>>>>>> eaf799c51f0009a30bb529771d0d01110e8bc46c
 #include <time.h>
 
 
-time_t time (time_t *second)
+time_t time (time_t *second);
 
 
 void timer_startTimer(void){
