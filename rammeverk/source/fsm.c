3b0233bfad75ddfc41eb2003f2f3349dce5f00fb
Haavaul <Haavaul@stud.ntnu.no>
2019-04-11 15:47:45 +0200

    Final changes estetic and adding stop-impementation for NOT_MOVING_BETWEEN_FLOORS, in fsm.c

--- rammeverk/source/fsm.c
+++ rammeverk/source/fsm.c
@@ -8,26 +8,27 @@
 #include <stdio.h>
 /**
 	@file
-	@brief C-file to make elevator state machine.
-*/
-/**
-	@brief Sets Position from "on floor" to "between floors"
+	@brief C-file to make the finite state machine for the elevator.
 */
 
+/**
+	@brief Changes Position from being on a floor to being between floors.
+*/
 void fsm_change_position_between_floors() {
     switch (elev_get_motor_direction()) {
         case DIRN_DOWN:
-        	orders_set_position(orders_get_position()+3);
+        	orders_set_position(orders_get_position() + N_FLOORS - 1);
             break;
 
         case DIRN_UP:
-        	orders_set_position(orders_get_position()+4);
+        	orders_set_position(orders_get_position() + N_FLOORS);
             break;
         default:
             printf("MotorDirection er satt til DIRN_STOP selv om du vil endre position til noe mellom etasjer.");
             break;
     }
 }
+
 /**
 	@brief Checks if elevator should stop at the current floor.
 	@return 1 If elevator should stop.
@@ -47,11 +48,12 @@ int fsm_stop_at_floor() {
     }
     return 0;
 }
+
 /**
-	@brief Sets the most prioritized direction for the elevator. (UP/DOWN)
+	@brief Sets the prioritized direction for the elevator. (UP/DOWN)
 */
 void fsm_set_priority_direction(){
-	//Checks that the elvator is still. Secures that it wont change direction momentarily.
+	//Checks that the elevator is still. Secures that it wont change direction momentarily.
 	if (elev_get_motor_direction() != DIRN_STOP){
 		return;
 	}
@@ -64,7 +66,7 @@ void fsm_set_priority_direction(){
 		elev_set_motor_direction(DIRN_DOWN);
 		return;
 	}
-	//If elev_get_last_moving_direction() == DIRN_DOWN
+	//Will only run if elev_get_last_moving_direction() == DIRN_DOWN.
 	if(orders_order_below_position()){
 		elev_set_motor_direction(DIRN_DOWN);
 		return;
@@ -75,12 +77,10 @@ void fsm_set_priority_direction(){
 
 
 void fsm_state_switch(){
-	printf("CurrentState: ");
-	printf("%d", CurrentState);
-	printf("\n");
 	switch (CurrentState){
 
-        case WAITING_FOR_INIT:
+        case WAITING_AFTER_INIT:
+				//Check for orders or stop signal.
         	while(!orders_exist_orders()){
         		if(elev_get_stop_signal()){
         			CurrentState=EMERGENCY_STOP;
@@ -88,6 +88,7 @@ void fsm_state_switch(){
         		}
         	orders_update_order_matrix();
         	}
+				//Check for orders above the current position. (Elevator is now in first floor.)
         	if(orders_order_above_position()){
         		CurrentState=MOVING;
         		return;
@@ -100,17 +101,20 @@ void fsm_state_switch(){
 			orders_delete_orders_at_this_floor(elev_get_floor_sensor_signal());
 			elev_set_door_open_lamp(1);
 			timer_start_timer();
+			//Check for orders or stop signal for 3 seconds.
 			while(timer_timer_expired(3.0) == 0){
 				if(elev_get_stop_signal()){
 					CurrentState=EMERGENCY_STOP;
 					return;
 				}
+				//If orders at the same floor, start case NOT_MOVING_AT_FLOOR again.
 				orders_update_order_matrix();
-				if (orders_order_at_this_floor(orders_get_position())){
+				if(orders_order_at_this_floor(orders_get_position())){
 					return;
 				}
 			}
 			elev_set_door_open_lamp(0);
+			//Check for orders or stop signal.
 			while(orders_exist_orders() == 0){
 				orders_update_order_matrix();
 				if (elev_get_stop_signal()){
@@ -118,19 +122,21 @@ void fsm_state_switch(){
 					return;
 				}
 			}
-			//FSM enters the same state "NOT_MOVING_AT_FLOOR" if there is a new order at the same floor
-            if (orders_order_at_this_floor(orders_get_position())){
+			//FSM enters the same state "NOT_MOVING_AT_FLOOR" if there is a new order at the same floor.
+  			if (orders_order_at_this_floor(orders_get_position())){
 				CurrentState = NOT_MOVING_AT_FLOOR;
 				return;
 			}
-			 	CurrentState = MOVING;
+		 	CurrentState = MOVING;
 			return;
 
-        case MOVING:
-        	fsm_set_priority_direction();
-            if (orders_get_position() < N_FLOORS) {
-                fsm_change_position_between_floors();
+    	case MOVING:
+    		fsm_set_priority_direction();
+			//If Position is at floor, change to between floors, depending on MotorDirection.
+          	if (orders_get_position() < N_FLOORS) {
+              fsm_change_position_between_floors();
             }
+			//Check for orders or stop signal.
        		while(elev_get_floor_sensor_signal() == -1) {
 				orders_update_order_matrix();
 				if(elev_get_stop_signal()){
@@ -138,18 +144,18 @@ void fsm_state_switch(){
 	                return;
 				}
 			}
-
 			CurrentState=AT_FLOOR;
 			return;
 
         case AT_FLOOR:
             orders_set_position(elev_get_floor_sensor_signal());
 			elev_set_floor_indicator(orders_get_position());
-
+			//Check if the elevator should stop at the current floor.
 			if (fsm_stop_at_floor()){
 				CurrentState = NOT_MOVING_AT_FLOOR;
 				return;
 			}
+			//Check for orders or stop signal.
 			while(elev_get_floor_sensor_signal() != -1){
 				if (elev_get_stop_signal()){
 					CurrentState = EMERGENCY_STOP;
@@ -164,15 +170,19 @@ void fsm_state_switch(){
 			elev_set_stop_lamp(1);
 			elev_set_motor_direction(DIRN_STOP);
 			orders_delete_all_orders();
-			if (orders_get_position()<4){ //at floor
+			//Check if elevator is at a floor
+			if (orders_get_position()<4){
 				elev_set_door_open_lamp(1);
+				//Wait for stop button to be released.
 				while(elev_get_stop_signal()){
 					//do nothing
 				}
-				CurrentState=NOT_MOVING_AT_FLOOR;
 				elev_set_stop_lamp(0);
+				CurrentState=NOT_MOVING_AT_FLOOR;
 				return;
 			}
+			//If elevator has stopped between floors.
+			//Wait for stop button to be released.
 				while(elev_get_stop_signal()){
 					//do nothing
 				}
@@ -181,8 +191,13 @@ void fsm_state_switch(){
 			return;
 
 		case NOT_MOVING_BETWEEN_FLOORS:
-			while((orders_exist_orders() == 0) && (elev_get_stop_signal() == 0)){
+			//Check for orders and stop signal.
+			while(orders_exist_orders() == 0){
 				orders_update_order_matrix();
+				if (elev_get_stop_signal()){
+					CurrentState = EMERGENCY_STOP;
+					return;
+				}
 			}
 			CurrentState = MOVING;
 			return;
--- rammeverk/source/fsm.h
+++ rammeverk/source/fsm.h
@@ -2,23 +2,23 @@
 #define FSM_H
 /**
 	@file
-	@brief H-file to make elevator state machine
+	@brief H-file to make the finite state machine for the elevator.
 */
 
 /**
- @brief enum to define the different states of the finite statemachine
+ @brief Enum to define the different states of the finite state machine.
  */
-typedef enum {WAITING_FOR_INIT, NOT_MOVING_AT_FLOOR, MOVING, AT_FLOOR, EMERGENCY_STOP, NOT_MOVING_BETWEEN_FLOORS} state;
+typedef enum {WAITING_AFTER_INIT, NOT_MOVING_AT_FLOOR, MOVING, AT_FLOOR, EMERGENCY_STOP, NOT_MOVING_BETWEEN_FLOORS} state;
 
 /**
- @brief changes between states, and defines what to do in each state
+ @brief Changes between states, and defines what to do in each state.
 */
 void fsm_state_switch(void);
 
 /**
- @brief Private variable containing the statemachines current state
+ @brief Private variable containing the state machine's current state.
  */
-static state CurrentState=WAITING_FOR_INIT;
+static state CurrentState=WAITING_AFTER_INIT;
 
 
 #endif
